2440806eba63554df04a774e59e0cfd9873cf223
CrazyETBoy <admin@mizz.app>
2020-05-20 11:44:42 +0800

    Updates

--- precache-manifest.4635a71dfa644810a6a95100177b4205.js
+++ precache-manifest.4635a71dfa644810a6a95100177b4205.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64ad924c4500cc27bd94fd90b6b0216a",
+    "revision": "b8e6c04feacdd017f7cce5d9476be6e7",
     "url": "./index.html"
   },
   {
-    "revision": "23d553577374ac5fe347",
+    "revision": "70ff5a9e1ae3f5069991",
     "url": "./static/css/main.1359096b.chunk.css"
   },
   {
-    "revision": "28734d2f8406b7e01dac",
-    "url": "./static/js/2.265d1075.chunk.js"
+    "revision": "1d44578354cd31a2d9a3",
+    "url": "./static/js/2.f1d4a4db.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "./static/js/2.265d1075.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.f1d4a4db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "23d553577374ac5fe347",
-    "url": "./static/js/main.1fbd087b.chunk.js"
+    "revision": "70ff5a9e1ae3f5069991",
+    "url": "./static/js/main.246d507f.chunk.js"
   },
   {
     "revision": "fdc570490cae7f61d1bd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e49269858e5cb2048866e7af397d7a13.js"
+  "./precache-manifest.4635a71dfa644810a6a95100177b4205.js"
 );
 
 self.addEventListener('message', (event) => {
